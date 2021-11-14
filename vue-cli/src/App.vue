d6d79870ff92efd162a28a10bd7246cec9c7b4a5
spring0913 <bbomi0913@gmail.com>
2021-11-14 23:14:57 +0900

    Study - 3

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data: function() {
+    return {
+      str: 'hi'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
