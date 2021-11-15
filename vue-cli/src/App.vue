10283ecb83d883f0fbd72286f6f033af80b42e65
spring0913 <bbomi0913@gmail.com>
2021-11-15 16:47:31 +0900

    Study - 4

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <app-header></app-header>
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
@@ -10,11 +11,16 @@ import AppHeader from './components/AppHeader.vue';
 export default {
   data: function() {
     return {
-      str: 'hi'
+      str: 'Header'
     }
   },
   components: {
     'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi';
+    }
   }
 }
 </script>
