c719447be2f00b78aad0ee3dfe018039729b57b5
spring0913 <bbomi0913@gmail.com>
2021-11-17 23:01:10 +0900

    Vue Router 추가

--- vue-trello/src/App.vue
+++ vue-trello/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    {{ msg }}
+    <router-view></router-view>
   </div>
 </template>
 
@@ -9,7 +9,6 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
