a9ad677ee3e7d17dc016221439b6cb132b7a5355
spring0913 <bbomi0913@gmail.com>
2021-11-23 19:21:09 +0900

    board 조회 화면- 1

--- vue-trello/src/store/actions.js
+++ vue-trello/src/store/actions.js
@@ -2,7 +2,7 @@ import * as api from '../api'
 
 const actions = {
     ADD_BOARD(_, {title}) {
-        return api.board.create(title)
+        return api.board.create(title).then(data => data.item)
     },
     FETCH_BOARDS ({commit}) {
         return api.board.fetch().then(data => {
@@ -12,6 +12,11 @@ const actions = {
     LOGIN({commit}, {email, password}) {
         return api.auth.login(email, password)
             .then(({accessToken}) => commit('LOGIN', accessToken))
+    },
+    FETCH_BOARD ({commit}, {id}) {
+        return api.board.fetch(id).then(data => {
+            commit('SET_BOARD', data.item)
+        })
     }
 }
 
